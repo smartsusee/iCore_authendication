b98d8aaa84f249f9465c77701e8fb308baa6f25e
smartsusee <susee37432@gmail.com>
2025-01-24 00:49:19 +0530

    authentication userdata show

--- frontend/src/components/Prodect/User.js
+++ frontend/src/components/Prodect/User.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function User() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem('authToken'); // Remove token
+    navigate('/', { replace: true }); // Redirect to login
+  };
   return (
-    <div>User Page</div>
-  )
+    <>
+    <div>
+      user
+         <button onClick={handleLogout}>Logout</button>
+    </div>
+    </> )
 }
 
 export default User
